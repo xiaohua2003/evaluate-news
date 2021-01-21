b9e78b920cedf2fa327629f02300776f8a5c7490
xiaohua2003 <72715756+xiaohua2003@users.noreply.github.com>
2021-01-20 22:01:44 -0700

    first commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
 
+const app = express()
+var textapi = new meaningCloud({
+    application_key: process.env.API_KEY
+})
+console.log(`Your API key is ${process.env.API_KEY}`);
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
