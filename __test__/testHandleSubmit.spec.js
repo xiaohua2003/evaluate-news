72caae300361f6962972bca088837aecb22aeaf9
xiaohua2003 <72715756+xiaohua2003@users.noreply.github.com>
2021-01-25 19:43:49 -0700

    first commit

--- __test__/testHandleSubmit.spec.js
+++ __test__/testHandleSubmit.spec.js
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import { handleSubmit } from "../src/client/js/formHandler"
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
